9cf237e7a4caf057a0f5b83960b48087ae6b9132
vladhoncharenko <vladhoncharenko@gmail.com>
2016-03-28 11:38:51 +0300

    ResultSetDisplay class was added

---
+++ src/main/java/dataBaseUtils/ResultSetDisplay.java
@@ -0,0 +1,73 @@
+package dataBaseUtils;
+
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+/**
+ * Allows to work with ResultSet
+ */
+
+public class ResultSetDisplay {
+
+    /**
+     * Method displays ResultSet content in the table.
+     *
+     * @param resultSet ResultSet
+     * @param out PrintWriter
+     */
+    public static void display(ResultSet resultSet, PrintWriter out) {
+
+        ResultSetMetaData resultSetMetaData;
+
+        try {
+
+            resultSetMetaData = resultSet.getMetaData();
+            int colCount = resultSetMetaData.getColumnCount();
+
+            //print column names
+            out.println("<table border='1'><tr>");
+            for (int i = 1; i <= colCount; ++i) {
+
+                out.println("<th>" + resultSetMetaData.getColumnName(i) + "</th>");
+            }
+
+            out.println("</tr>");
+            //print data
+            while (resultSet.next()) {
+
+                out.println("<tr>");
+
+                for (int i = 1; i <= colCount; ++i)
+                    out.println("<td>" + resultSet.getString(i) + "</td>");
+
+                out.println("</tr>");
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns rows amount in ResultSet
+     *
+     * @param resultSet
+     * @return rows amount in ResultSet
+     */
+    public static int getSelectedRowsAmount(ResultSet resultSet) {
+        int amount = 0;
+        try {
+            while (resultSet.next()) {
+                amount++;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return amount;
+
+    }
+}
+
