78a7e6f38434f5ddfc6f3deb4f8cfe86270ef44f
vladhoncharenko <vladhoncharenko@gmail.com>
2016-03-30 17:57:22 +0300

    Control and View in addDeptntServlet were separated

--- src/main/java/servlets/addDeptntServlet.java
+++ src/main/java/servlets/addDeptntServlet.java
@@ -31,30 +31,14 @@ public class addDeptntServlet extends HttpServlet {
             e.printStackTrace();
         }
 
+        requestDispatcher = request.getRequestDispatcher("addDept.jsp");
+        try {
+            requestDispatcher.include(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<html><head><title>Add Department</title></head><body>");
-
-        out.println("<form method=\"post\" action=\"/lab3/departmentadd\">");
-        out.println("<h2>To add new department: Enter data in fields and press \"Add\"</h2>");
-
-        out.println("<label for=\"dnameId\">Enter Name:</label>");
-        out.println("<div><input type=\"text\" name=\"dname\" value=\"\" size=\"20\" id=\"dnameId\"" +
-                " data-validation=\"length\" data-validation-length=\"1-13\"" +
-                " data-validation-error-msg=\"Data is not valid. Please, enter 1-13 symbols.\" /></div>");
-        out.println("<label for=\"locId\">Enter Location:</label>");
-        out.println("<div><input type=\"text\" name=\"loc\" value=\"\" size=\"20\" id=\"locId\" " +
-                "data-validation=\"length\" data-validation-length=\"1-13\"" +
-                "data-validation-error-msg=\"Data is not valid. Please, enter 1-13 symbols.\" /></div>");
-
-        out.println("<input type=\"submit\" name=\"RUNb\" value=\"Add\"/>");
-        out.println("</form>");
-        out.println("<script type=\"text/javascript\" src=\"//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script>");
-        out.println("<script type=\"text/javascript\" src=\"//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.2.8/jquery.form-validator.min.js\"></script>");
-        out.println("<link href=\"//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.2.8/theme-default.min.css\"\n" +
-                "    rel=\"stylesheet\" type=\"text/css\" />");
-        out.println("<script>$.validate();</script>");
-
         int result = 0;
         String query = null;
         query = ("INSERT INTO DEPTNT(DNAME,LOC) VALUES('" + request.getParameter("dname") + "','" + request.getParameter("loc") + "')");
@@ -66,7 +50,6 @@ public class addDeptntServlet extends HttpServlet {
             result = ExecutePLSQL.executeUpdate(query);
             out.println("<p>" + result + " row(s) added</p>");
         }
-        out.println("</body></html>");
         out.close();
 
         requestDispatcher = request.getRequestDispatcher("footer.jsp");
---
+++ src/main/webapp/addDept.jsp
@@ -0,0 +1,32 @@
+<%--
+  View of add Department Servlet
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Add Department</title>
+</head>
+<body>
+
+<form method="post" action="/lab3/departmentadd">
+    <h2>To add new department: Enter data in fields and press "Add"</h2>
+
+    <label for="dnameId">Enter Name:</label>
+    <div><input type="text" name="dname" value="" size="20" id="dnameId" data-validation="length"
+                data-validation-length="1-13"
+                data-validation-error-msg="Data is not valid. Please, enter 1-13 symbols."/></div>
+    <label for="locId">Enter Location:</label>
+    <div><input type="text" name="loc" value="" size="20" id="locId" data-validation="length"
+                data-validation-length="1-13"
+                data-validation-error-msg="Data is not valid. Please, enter 1-13 symbols."/></div>
+
+    <input type="submit" name="RUNb" value="Add"/>
+</form>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript"
+        src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.2.8/jquery.form-validator.min.js"></script>
+<link href="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.2.8/theme-default.min.css" rel="stylesheet"
+      type="text/css"/>
+<script>$.validate();</script>
+</body>
+</html>
