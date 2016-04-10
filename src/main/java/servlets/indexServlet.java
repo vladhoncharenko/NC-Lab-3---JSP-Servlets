6a265a7cb80cecaa31e314f4f399fe2bc9c67e0c
vladhoncharenko <vladhoncharenko@gmail.com>
2016-04-10 14:19:42 +0300

    JavaDoc  was added

--- src/main/java/servlets/indexServlet.java
+++ src/main/java/servlets/indexServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+/**
+ * Index servlet
+ */
 @WebServlet(name = "IndexServlet")
 public class IndexServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/java/servlets/loginRegisterServlet.java
+++ src/main/java/servlets/loginRegisterServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+/**
+ * Login/Register Servlet
+ */
 @WebServlet(name = "LoginRegisterServlet")
 public class LoginRegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
